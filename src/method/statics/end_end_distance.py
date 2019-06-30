correct weighting problem

src/method/statics/end_end_distance.py
@@ -28,8 +28,8 @@ class end_end_distance(Analysis):
         self.fulltrj=fulltrj
         #constructor:
         if self.fulltrj==False:
-            self.weighting=np.zeros(self.List.System.get_NumberBlocks+1)
-            self.EndEndDistance=np.zeros(self.List.System.get_NumberBlocks+1)
+            self.weighting=np.zeros(self.List.System.get_NumberBlocks)
+            self.EndEndDistance=np.zeros(self.List.System.get_NumberBlocks)
         elif self.fulltrj==True:
             self.weighting=np.zeros(self.List.System.get_NumberSteps)
             self.EndEndDistance=np.zeros(self.List.System.get_NumberSteps,dtype=np.float32)

src/method/statics/gyration_tensor.py
@@ -29,8 +29,8 @@ class gyration_tensor(Analysis):
 
         self.fulltrj=fulltrj
         if fulltrj==False:
-            self.gyrationtensor=np.zeros(self.List.System.get_NumberBlocks+1,dtype=np.ndarray)
-            self.weighting=np.zeros(self.List.System.get_NumberBlocks+1)
+            self.gyrationtensor=np.zeros(self.List.System.get_NumberBlocks,dtype=np.ndarray)
+            self.weighting=np.zeros(self.List.System.get_NumberBlocks)
         elif fulltrj==True:
             self.gyrationtensor=np.zeros(self.List.System.sys_NumberSteps,dtype=np.ndarray)
             self.weighting=np.zeros(self.List.System.sys_NumberSteps,dtype=np.ndarray)